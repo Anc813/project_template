b9d9dacc16c9e265de65aab4f344b14a64702f92
Anc813 <anc813@gmail.com>
2017-02-05 10:57:28 +0200

    Update base.py

--- project/core/settings/base.py
+++ project/core/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     # 'rest_framework',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
